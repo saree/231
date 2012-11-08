7430a338459a98e1c62ecb787bb310fcd9c04654
Saree Costa <saree.costa@gmail.com>
2012-11-08 14:01:41 -0800

    Adding files

---
+++ test.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+<title>The Cooking Gurus: College Connosieurs Conjuring Up Healthy Concoctions</title>
+
+<style type="text/css">
+div#navigation {
+   background-color:black;
+   color:white;
+   width:800px;
+   height:80px;
+   margin-left:100px;
+}
+div#content {
+   background-color:black;
+   color:white;
+   width:800px;
+   height:80px;
+   margin-left:100px;
+}
+</style>
+</head>
+
+<body>
+<div id="container">
+
+ <div id="header">
+  <h1>The Cooking Gurus</h1>
+ </div>
+
+ <div id="navigation">
+   <ul>
+     <li>Home</li>
+     <li>Video Tutorials</li>
+     <li>Resources</li>
+     <li>Contact</li>
+   </ul>
+
+ </div> <!-- End Navigation -->
+
+
+  <div id="main-content">
+  </div>
+
+</div> <!-- End Container -->
+</body>
+
+</html>
